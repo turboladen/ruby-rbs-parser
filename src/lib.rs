1ef5b7fcb77b3c47d179f9fba8d73bf567146d8c
Steve Loveless <steve.loveless@gmail.com>
2024-09-04 23:12:05 -0700

    Remove nom things

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,60 @@
-// pub mod nom_parser;
-pub mod pest_parser;
-
 #[derive(pest_derive::Parser)]
 #[grammar = "rbs.pest"]
 pub struct RbsParser;
+
+// use pest::{error::Error, iterators::Pairs, pratt_parser::PrattParser, Parser as _};
+use pest::{error::Error, iterators::Pairs, Parser as _};
+
+// lazy_static::lazy_static! {
+//     static ref PRATT_PARSER: PrattParser<Rule> = {
+//         use pest::pratt_parser::{Assoc::{Left, Right}, Op};
+//         // use Assoc::*;
+//         // use Rule::*;
+//
+//         PrattParser::new()
+//             .op(Op::postfix(Rule::namespace_op))
+//     };
+// }
+//
+
+pub fn parse_class(source: &str) -> Result<Pairs<Rule>, Error<Rule>> {
+    crate::RbsParser::parse(Rule::class_decl, source)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn class_decl_no_members_positive_test() {
+        let text = "class Foo\nend";
+        let _ = parse_class(text).unwrap();
+
+        let text = "class Foo[T]\nend";
+        let _ = parse_class(text).unwrap();
+
+        let text = "class Foo[T, U]\nend";
+        let _ = parse_class(text).unwrap();
+
+        let text = "class Foo[out T]\nend";
+        let _ = parse_class(text).unwrap();
+
+        let text = "class Foo[in T]\nend";
+        let _ = parse_class(text).unwrap();
+
+        let text = "class Foo[unchecked T]\nend";
+        let _ = parse_class(text).unwrap();
+
+        let text = "class Foo[unchecked out T]\nend";
+        let _ = parse_class(text).unwrap();
+
+        let text = "class Foo[unchecked in T]\nend";
+        let _ = parse_class(text).unwrap();
+
+        let text = "class Foo[T < _Output]\nend";
+        let _ = parse_class(text).unwrap();
+
+        let text = "class Foo[T] < Bar[T]\nend";
+        let _ = parse_class(text).unwrap();
+    }
+}
