7e792a96dbd0149f650828584a4f0e4df70c671a
Steve Loveless <steve.loveless@gmail.com>
2024-09-04 22:53:29 -0700

    Rename to ruby-rbs-parser

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use pest::Parser as _;
-use rbsby::{RbsParser, Rule};
+use ruby_rbs_parser::{RbsParser, Rule};
 
 fn main() {
     let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
 
-    // println!("{}", src);
-    // println!("{}", chum::parser().parse(src));
     let output = RbsParser::parse(Rule::rtype, &src);
     println!("{:#?}", output);
 
-    let output = rbsby::pest_parser::parse_class(&src);
+    let output = ruby_rbs_parser::pest_parser::parse_class(&src);
     println!("{:#?}", output);
 }
