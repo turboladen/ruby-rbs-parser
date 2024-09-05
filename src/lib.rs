569326d473047afd63137aa34ff447f6ae4125ad
Steve Loveless <steve.loveless@gmail.com>
2024-09-04 23:33:52 -0700

    Add parse_module_decl

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,11 @@ pub fn parse_class_decl(source: &str) -> Result<Pairs<Rule>, Error<Rule>> {
     crate::RbsParser::parse(Rule::class_decl, source)
 }
 
+pub fn parse_module_decl(source: &str) -> Result<Pairs<Rule>, Error<Rule>> {
+    crate::RbsParser::parse(Rule::module_decl, source)
+}
+
+// NOTE: FYI: https://github.com/ruby/rbs/blob/master/test/rbs/parser_test.rb
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -56,5 +61,38 @@ mod tests {
 
         let text = "class Foo[T] < Bar[T]\nend";
         let _ = parse_class_decl(text).unwrap();
+    }
+
+    #[test]
+    fn module_decl_no_members_positive_test() {
+        let text = "module Foo\nend";
+        let _ = parse_module_decl(text).unwrap();
+
+        let text = "module Foo[T]\nend";
+        let _ = parse_module_decl(text).unwrap();
+
+        let text = "module Foo[T, U]\nend";
+        let _ = parse_module_decl(text).unwrap();
+
+        let text = "module Foo[out T]\nend";
+        let _ = parse_module_decl(text).unwrap();
+
+        let text = "module Foo[in T]\nend";
+        let _ = parse_module_decl(text).unwrap();
+
+        let text = "module Foo[unchecked T]\nend";
+        let _ = parse_module_decl(text).unwrap();
+
+        let text = "module Foo[unchecked out T]\nend";
+        let _ = parse_module_decl(text).unwrap();
+
+        let text = "module Foo[unchecked in T]\nend";
+        let _ = parse_module_decl(text).unwrap();
+
+        let text = "module Foo[T < _Output]\nend";
+        let _ = parse_module_decl(text).unwrap();
+
+        let text = "module Foo[T] : String, _Array[Symbol]\nend";
+        let _ = parse_module_decl(text).unwrap();
     }
 }
